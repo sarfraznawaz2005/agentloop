94302c97b2607d22e533dc7bb8874ffa64184a95
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2026-02-20 18:52:33 +0500

    feat: add new properties to agent option model

--- src/AgentLoop.Data/Models/AgentOption.cs
+++ src/AgentLoop.Data/Models/AgentOption.cs
@@ -4,4 +4,6 @@ public class AgentOption
 {
     public string Name { get; set; } = string.Empty;
     public string Command { get; set; } = string.Empty;
+
+    public override string ToString() => Name;
 }
